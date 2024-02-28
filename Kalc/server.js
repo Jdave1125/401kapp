079f281c4220749775f54c59395c899ee672c466
Jdave1125 <Jdave1125@gmail.com>
2024-02-28 18:22:16 -0500

    4kappeu4m

--- Kalc/server.js
+++ Kalc/server.js
@@ -78,5 +78,5 @@ app.post('/send-report', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5004;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- Kalc/src/ViewReport.jsx
+++ Kalc/src/ViewReport.jsx
@@ -11,7 +11,7 @@ function ViewReport({ userInput, totalBalance, chartData, options }) {
   const handleEmailReport = () => {
 
 
-  fetch('https://401kapp.vercel.app/send-report',{
+  fetch('https://401kapp-eu4m.vercel.app/send-report',{
     method:'POST',
     headers:{
       'Content-Type':'application/json',
