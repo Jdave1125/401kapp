95fcf0ccc20ba70847deee7a9a8f0cf0efd7857f
Jdave1125 <Jdave1125@gmail.com>
2024-02-29 19:45:39 -0500

    conditional title

--- Kalc/src/navbar.jsx
+++ Kalc/src/navbar.jsx
@@ -1,12 +1,17 @@
-// Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <nav>
       <div className='nav-container'>
-        <Link to="/"  className='navTitle'>401Kalc</Link>
+        {location.pathname === '/view-report' ? (
+          <Link to="/" className='navTitle'>Return Home</Link>
+        ) : (
+          <Link to="/" className='navTitle'>401Kalc</Link>
+        )}
         <p className='navP'>
           Calculate your estimated 401k balance at any time
         </p>
